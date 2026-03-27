1916a9d1bb454acea0514730bf5fe12d760892b1
B-Ricey763 <bhanna30@gatech.edu>
2026-03-26 22:29:50 -0700

    feat(nvim): added MiniMax and customized
    Added nvim minimax: https://nvim-mini.org/MiniMax/ and customized some
    need to haves from my other config. Will be testing in the future

--- shared/nvim/lua/plugins/scissors.lua
+++
@@ -1,8 +0,0 @@
--- Used to dynamically add snippets on the fly
-return {
-  'chrisgrieser/nvim-scissors',
-  dependencies = 'folke/snacks.nvim',
-  opts = {
-    snippetDir = vim.fn.stdpath 'config' .. '/snippets',
-  },
-}
